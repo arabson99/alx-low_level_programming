a247c303cdd518b612eaed2f236b05dbde1a967f
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-08 15:22:09 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+1 #include <stdio.h>
+
+/*
+ * main - print "Programming is like building a multilingual puzzle"
+ * return :0
+ */
+
+int main ()
+{
+	puts("\"programming is like building a multilingual puzzel");
+	return 0;
+}	
+
