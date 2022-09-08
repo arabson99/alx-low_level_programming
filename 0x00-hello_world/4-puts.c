a50c57ebfae0708f73ba0b8acf8b3c3d6a714cdf
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-08 15:42:21 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-/**
- * main - print "Programming is like building a multilingual puzzle"
- *
- * return : 0 after printing
- */
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzel");
-	return (0);
-}	
 
+
+/**
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
