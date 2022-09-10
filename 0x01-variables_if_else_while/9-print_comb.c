f46a54ed60db27e50fffef6cb2af036cc3e8c101
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-09 20:43:30 -0700

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n <= 58; n++)
 	{
 		putchar(n);
 	}
-		if (n != 57)
+		if (n != 58)
 		{
 			putchar(',');
 			putchar(' ');
