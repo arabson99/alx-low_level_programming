5b46c8941a5401d62d75f3a0553a0e5d8c04776d
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-09 21:39:35 -0700

    invention

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int n, m;
 	for (n = 48; n <= 56; n++)
 	{
-		for (m = 49; m <= 57; m++)
+		for (m = 49; m <= 56; m++)
 		{
 			if (m > n)
 			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
+				putchar(n + 0);
+				putchar(m + 0);
+				if (n != 56)
 				{
 					putchar(',');
 					putchar(' ');
