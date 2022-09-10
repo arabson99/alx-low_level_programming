8d2aa2e04e9d3ad593b86f2fd238cd7f69dd19f5
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-09 20:14:16 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,9 +12,13 @@ int main(void)
 	char low;
 
 	for (d = 48; d < 58; d++)
+	{
 		putchar(d);
+	}
 	for (low = 'a'; low <= 'f'; low++)
+	{
 		putchar(low);
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
