9aa583b2212b72d9f4b7b5995b39fc6c889ea762
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-15 16:34:10 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,22 +9,21 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for ((j = size - i); j > 0; j--)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
-				for (j = 0; j < i; j++)
-				{
-					_putchar('#');
-				}
-
-				if (i == size)
-				{
-					continue;
-				}
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
