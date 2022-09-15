b1fd4cd807736cf4e0fb8d3645a07a6c19b2324e
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-15 16:21:51 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include "main,h"
+
+/**
+ * main - prints the largest prime factor of a number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long number = 612852475143;
+	int i;
+
+	while (i++ < number / 2)
+	{
+		if (number % i == 0)
+		{
+			number /= 2;
+			continue;
+		}
+
+		for (i = 3; i < number / 2; i += 2)
+		{
+			if (number % i == 0)
+				number /= i;
+		}
+	}
+	printf("%ld\n", number);
+	return (0);
+}
