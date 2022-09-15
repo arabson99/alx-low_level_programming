7b03acead4475c9e06b1363bf4b65f336b37c1af
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-15 01:57:46 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,9 @@
 void times_table(void)
 {
 	int i, j, product;
-	
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
-		
 		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
@@ -21,7 +19,6 @@ void times_table(void)
 			product = i * j;
 			if (product <= 9)
 				_putchar(' ');
-
 			else
 				_putchar((product / 10) + '0');
 
