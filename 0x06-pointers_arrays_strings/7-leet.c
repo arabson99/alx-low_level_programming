dc19d0f8d9eaeb593798b9d261400e2bd783f093
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-22 15:34:03 -0700

    taask 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,19 @@
  */
 char *leet(char *str)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, j;
 
-	while (str[++index1])
+	char *a = "aAeEoOtTlL";
+	char *a = "aAeEoOtTlL";
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (j = 0; j < 10; j++)
 		{
-			if (str[index1] == leet[index2] ||
-			srt[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
 		}
 	}
 	return (str);
