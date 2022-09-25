2070abd8a0b8f80d202f2997515383858b6773be
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-25 13:57:25 -0700

    tast 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * jack bauer - func that prints every minute of the day of Jack Bauer, starting
- * from 00:00 to 23:59, min loop counts mins, while hour loop counts hours and resets mins
+ * jack_bauer - func that prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59, min loop counts mins,
+ * while hour loop counts hours and resets mins
  *
  * Return: 0
  */
@@ -28,5 +29,5 @@ void jack_bauer(void)
 	}
 	hours++;
 	minutes = 0;
-}
+	}
 }
