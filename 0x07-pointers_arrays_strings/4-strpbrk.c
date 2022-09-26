49b3777648c0b62325c70be5db7da0b5d27bef35
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-09-26 14:33:20 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string to check
- * @accept; string to check against
+ * @accept: string to check against
  *
  * Return: pointer to byte in s that matches or
  * NULL if ni match
