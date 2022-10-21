1008a60d5a7231843d7271ad278a5bce37814143
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-10-20 17:28:16 -0700

    fixed

--- 0x09-static_libraries/test.c
+++ 0x09-static_libraries/test.c
@@ -1,8 +1,12 @@
 #include "main.h"
-
+/**
+ * main - puts func
+ * Return: Always 0
+ */
 int main(void)
 {
-	_puts("\"At the endnof the day, my goal was to be the best hacker\"\n\t- Kevin mitnick");
+	_puts("\"At the endnof the day, my goal was to be
+			the best hacker\"\n\t- Kevin mitnick");
 
 	return (0);
 }
