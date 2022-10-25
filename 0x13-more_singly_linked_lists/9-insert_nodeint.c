6c6caaa6422e0a7b22447ddcaff4b91f02dbde2b
Abdulazeez Usman Abubakar <arabiusman99@gmail.com>
2022-10-25 14:58:56 -0700

    fixedd

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,8 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node, *temp = *head;
+	listint_t *node;
+	listint_t *temp = *head;
 	unsigned int i;
 
 	node = malloc(sizeof(listint_t));
@@ -21,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		node->next == *head;
+		node->next = *head;
 		*head = node;
 		return (node);
 	}
